5bb0ab6b81329d461e271d88c458c0513572a4bb
Jimmy <qny123@naver.com>
2023-03-08 12:00:34 +0900

    feat: chakraProvider 적용 및 라우터 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
-function App() {
-  return <div>test</div>;
-}
+import '@fontsource/raleway/400.css';
+import '@fontsource/open-sans/700.css';
 
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+import { ChakraProvider } from '@chakra-ui/react';
+
+import { Main, Reservation } from './pages';
+import theme from './styles/theme';
+
+function App() {
+  return (
+    <ChakraProvider theme={theme}>
+      <Routes>
+        <Route path="/" element={<Navigate to="/main" />} />
+        <Route path="/main" element={<Main />} />
+        <Route path="/reservation" element={<Reservation />} />
+      </Routes>
+    </ChakraProvider>
+  );
+}
 export default App;
