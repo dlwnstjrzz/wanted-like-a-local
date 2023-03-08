acb7ad110f2815c27bc7d9e56ac0008fd68d252f
Jimmy <qny123@naver.com>
2023-03-08 12:05:08 +0900

    chore: import 편의를 위한 index파일 생성

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Footer } from './Footer';
+export { default as Nav } from './Nav';
+export { default as Layout } from './Layout';
+export { default as ProductCard } from './ProductCard';
