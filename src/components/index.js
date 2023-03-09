c847f0bad14ca1c33d90f239794723ee233f4e20
Jimmy <qny123@naver.com>
2023-03-09 18:00:33 +0900

    feat: 검색필터 컴포넌트에서 redux 전역상태로 저장해둔 값으로 main 컴포넌트에서 검색필터기능 구현

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
-export { default as Footer } from './Footer';
-export { default as Nav } from './Nav';
+export { default as ProductModal } from './ProductModal';
 export { default as Layout } from './Layout';
 export { default as ProductCard } from './ProductCard';
+export { default as SearchFilter } from './SearchFilter';
+export { default as ProductDetail } from './ProductDetail';
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,15 +1,39 @@
-import { Grid } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 
-import { Layout } from '../components';
-import ProductAddToCart from '../components/test';
+import { Flex, Grid } from '@chakra-ui/react';
+
+import { Layout, ProductCard, SearchFilter } from '../components';
 import productList from '../data/product_list.json';
 
 export default function Main() {
+  const { selectedCity, selectedPrice } = useSelector((state) => state.filter);
+  const [products, setProducts] = useState(productList);
+
+  useEffect(() => {
+    let filteredProducts = productList;
+    if (selectedCity) {
+      filteredProducts = filteredProducts.filter(
+        (product) => product.spaceCategory === selectedCity,
+      );
+    }
+    if (selectedPrice.maxPrice) {
+      filteredProducts = filteredProducts.filter(
+        (product) =>
+          product.price >= selectedPrice.minPrice && product.price <= selectedPrice.maxPrice,
+      );
+    }
+    setProducts(filteredProducts);
+  }, [selectedCity, selectedPrice]);
+
   return (
     <Layout>
+      <Flex width="100%" justify="flex-end" pt={15} pr={50}>
+        <SearchFilter />
+      </Flex>
       <Grid maxW="80%" templateColumns="repeat(2, 1fr)" gap={0} margin="auto">
-        {productList.map((product) => {
-          return <ProductAddToCart product={product} />;
+        {products?.map((product) => {
+          return <ProductCard key={product.idx} product={product} />;
         })}
       </Grid>
     </Layout>
